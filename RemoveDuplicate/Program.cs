03efa8a6dc684897d26d0facdcbfb52d6d1b4f2f
josevictormoreno <josevictor.admoreno@gmail.com>
2022-11-10 11:27:13 -0300

    remove duplicate char in a string

---
+++ RemoveDuplicate/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace RemoveDuplicate
+{
+  public class RemoveDuplicate
+  {
+    static void Main()
+    {
+      Console.WriteLine(RemoveDuplicates("abbaca"));
+    }
+
+    public static string RemoveDuplicates(string s)
+    {
+      string text = "";
+      for (int i = 0; i < s.Length- 1; i++)
+      {
+        if (i == 0)
+          text += s[i];
+        else
+        {
+          if (s[i] != text[i-1])
+            text += s[i];
+        }
+      }
+
+      return text;
+    }
+  }
+}
