5c3385789a807a3a611152a7b8acae48ebff3295
josevictormoreno <josevictor.admoreno@gmail.com>
2023-03-07 22:20:11 -0300

    Adiciona exercicio Customers who nerver order

--- sql/exercicios.sql
+++ sql/exercicios.sql
@@ -3,3 +3,6 @@ select email from person group by email having count(*) > 1
 
 -- Exercicio 175 Combine two table
 select p.firstName, p.lastName, a.city, a.state from Person p left join Address a on p.personId = a.personId
+
+-- Exercicio 183 Customers Who never order
+SELECT Name as Customers from Customers LEFT JOIN Orders ON Customers.Id = Orders.CustomerId WHERE Orders.CustomerId IS NULL;
